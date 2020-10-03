85ee91d95383f860710f43fd4518d3b3f65b30cd
Yogesh Padekar <ypadekar@gmail.com>
2020-10-04 01:56:43 +0530

    Added required files and created core data file

---
+++ Comments/Supporting Files/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Comments
+//
+//  Created by Yogesh Padekar on 03/10/20.
+//  Copyright © 2020 Yogesh. All rights reserved.
+//
+
+struct CommentsConstants {
+    static let APILink = "https://jsonplaceholder.typicode.com/comments"
+    static let Comment = "Comment"
+}
+
+struct CommentDetailConstants {
+    static let Email = "Email: "
+    static let PostId = "Post Id: "
+    static let CommentId = "Comment Id: "
+    static let Title = "Comment Title:\n"
+    static let Body = "Comment Body:\n"
+}
