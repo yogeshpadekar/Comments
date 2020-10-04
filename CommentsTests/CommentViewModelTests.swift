7e95a4984a3879d4a396dbcb87a0a040c8829cac
Yogesh Padekar <ypadekar@gmail.com>
2020-10-04 17:19:17 +0530

    Written test cases for CommentViewModel
    All properties of the view model are tested.

---
+++ CommentsTests/CommentViewModelTests.swift
@@ -0,0 +1,88 @@
+//
+//  CommentViewModelTests.swift
+//  CommentsTests
+//
+//  Created by Yogesh Padekar on 02/10/20.
+//  Copyright © 2020 Yogesh. All rights reserved.
+//
+
+import XCTest
+@testable import Comments
+
+class CommentViewModelTests: XCTestCase {
+    lazy var comments: [Comment] = []
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let testBundle = Bundle(for: type(of: self))
+        if let testJSONURL = testBundle.url(forResource: "TestComments", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: testJSONURL)
+                if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+                    let responseDecoder = JSONDecoder()
+                    responseDecoder.userInfo[CodingUserInfoKey.managedObjectContext] = appDelegate.persistentContainer.viewContext
+                    self.comments = try responseDecoder.decode([Comment].self, from: data)
+                }
+            } catch {
+                debugLog("Error in fetching data from json file = \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.comments.removeAll()
+    }
+    
+    func testFetchingComments() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(self.comments.count, 10)
+    }
+    
+    
+    func testViewModelIds() {
+        let commentViewModel1 = CommentViewModel(comment: self.comments[1])
+        let commentViewModel2 = CommentViewModel(comment: self.comments[5])
+        let commentViewModel3 = CommentViewModel(comment: self.comments[8])
+        XCTAssertEqual(commentViewModel1.id, "2")
+        XCTAssertEqual(commentViewModel2.id, "6")
+        XCTAssertEqual(commentViewModel3.id, "9")
+    }
+    
+    func testViewModelPostIds() {
+        let commentViewModel1 = CommentViewModel(comment: self.comments[0])
+        let commentViewModel2 = CommentViewModel(comment: self.comments[4])
+        let commentViewModel3 = CommentViewModel(comment: self.comments[7])
+        XCTAssertEqual(commentViewModel1.postId, "1")
+        XCTAssertEqual(commentViewModel2.postId, "1")
+        XCTAssertEqual(commentViewModel3.postId, "2")
+    }
+    
+    func testViewModelEmails() {
+        let commentViewModel1 = CommentViewModel(comment: self.comments[2])
+        let commentViewModel2 = CommentViewModel(comment: self.comments[6])
+        let commentViewModel3 = CommentViewModel(comment: self.comments[8])
+        XCTAssertEqual(commentViewModel1.email, "Nikita@garfield.biz")
+        XCTAssertEqual(commentViewModel2.email, "Dallas@ole.me")
+        XCTAssertEqual(commentViewModel3.email, "Meghan_Littel@rene.us")
+    }
+    
+    func testViewModelTitles() {
+        let commentViewModel1 = CommentViewModel(comment: self.comments[3])
+        let commentViewModel2 = CommentViewModel(comment: self.comments[5])
+        let commentViewModel3 = CommentViewModel(comment: self.comments[9])
+        XCTAssertEqual(commentViewModel1.title, "alias odio sit")
+        XCTAssertEqual(commentViewModel2.title, "et fugit eligendi deleniti quidem qui sint nihil autem")
+        XCTAssertEqual(commentViewModel3.title, "eaque et deleniti atque tenetur ut quo ut")
+    }
+    
+    func testViewModelBodies() {
+        let commentViewModel1 = CommentViewModel(comment: self.comments[0])
+        let commentViewModel2 = CommentViewModel(comment: self.comments[1])
+        let commentViewModel3 = CommentViewModel(comment: self.comments[2])
+        XCTAssertEqual(commentViewModel1.body, "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium")
+        XCTAssertEqual(commentViewModel2.body, "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et")
+        XCTAssertEqual(commentViewModel3.body, "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione")
+    }
+    
+}
